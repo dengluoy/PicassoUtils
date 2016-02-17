269aec4a81f6b06bbc9b2c5c343bfa5e72e0df18
dengluoy <develhello@163.com>
2016-02-17 18:28:59 +0800

    first commit

---
+++ app/src/main/java/picassoutils/david/com/picassoutils/utils/PicassoUtils.java
@@ -0,0 +1,24 @@
+package picassoutils.david.com.picassoutils.utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * @author WeiDeng
+ * @date 16/2/16
+ * @description picasso工具类
+ */
+public class PicassoUtils {
+
+    public final String TAG = getClass().getSimpleName();
+
+    private static Picasso mInstance;
+
+
+    public static void initPicasso(@NonNull Context context) {
+        
+    }
+
+}
